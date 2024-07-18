dbdf8de60e955de23399bbdd3af84f406681daf6
Aya Dahbour <157162264+ayaDahbour@users.noreply.github.com>
2024-07-18 13:05:59 +0300

    Update error_script.sh

--- shell/error_script.sh
+++ shell/error_script.sh
@@ -1,33 +1,55 @@
-
 #!/bin/bash
 
-# !/bin/bash
+echo "Starting script..."
 
-MY_VAR = Hello, World
 
-echo `MY_VAR`
+MY_VAR="Hello, World" # Missed double quotes
 
-for i in 1 2 3
+# Using "" not ''
+echo "$MY_VAR"
+
+echo "Entering for loop..."
+
+for i in 1 2 3; 
+do # Missed "do"
   echo "Number: $i"
+done
+echo "For loop completed."
 
-FILE_NAME=some file with spaces.txt
-if [ -f $FILE_NAME ]; then
+# Quoting file name
+FILE_NAME="aya dahbour.txt"
+echo "FILE_NAME is set to: $FILE_NAME"
+
+if [ -f "$FILE_NAME" ]; then # "$FILE_NAME"
     echo "File exists."
 else
     echo "File does not exist."
 fi
 
-if [ -d "/some/nonexistent/dir" ]; then
+# DIR_PATH assignment 
+DIR_PATH="/home/aya/Desktop/task11"
+echo "DIR_PATH is set to: $DIR_PATH"
+
+if [ -d "$DIR_PATH" ]; then
     echo "Directory exists."
-    else
+else
     echo "Directory does not exist."
 fi
 
-unavailable_command --version
+echo "Running version..."
+lsb_release -a
 
 CURRENT_DATE=$(date +%Y-%m-%d)
+echo "CURRENT_DATE is set to: $CURRENT_DATE"
 
-echo "Some content" > /etc/readonlyfile
+# Writing to a safe location
+echo "Attempting to write to a file..."
+echo "Some content" > ~/test.txt 
+echo "Write operation completed."
 
+# Evaluating user input command
 USER_INPUT="ls -l"
+echo "USER_INPUT is set to: $USER_INPUT"
 eval $USER_INPUT
+
+echo "Script completed."
